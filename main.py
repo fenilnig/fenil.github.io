8f766ab275aa4207b8ab7af5c459112c7942a816
Fenil Shah <shahv1632@gmail.com>
2026-03-04 00:55:24 +0530

    Project done

---
+++ main.py
@@ -0,0 +1,12 @@
+from add import add
+from sub import sub
+from mul import mul
+from div import div
+
+a = float(input())
+b = float(input())
+
+print("Add:", add(a, b))
+print("Sub:", sub(a, b))
+print("Mul:", mul(a, b))
+print("Div:", div(a, b))
